746970cd7e87a82b3b0577ef1692c79792ed2f98
Adrian Sieh <adrian.sieh@gmail.com>
2021-01-28 19:54:44 -0500

    Added straight driving code from 2018 and tested to see if it moved back to where it started. It was very close to the original position.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UltimateAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UltimateAuton.java
@@ -2,12 +2,19 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 @Autonomous(name="Auton", group="Pushbot")
 public class UltimateAuton extends LinearOpMode {
 
     /* Declare OpMode members. */
     UltimateHardware robot = new UltimateHardware();   // Use a Pushbot's hardware
+    double forward = 0.5;
+    double right = 0;
+    double intRunTime = 0;
+    int msToRun = 2500;
+
+    ElapsedTime runtime = new ElapsedTime();
 
     @Override
     public void runOpMode() {
@@ -27,8 +34,32 @@ public class UltimateAuton extends LinearOpMode {
 
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
-
-            telemetry.update();
+            timedDrive(forward, right, msToRun);
+            timedDrive(0, 0, 1000);
+            timedDrive(forward*-1, right, msToRun);
+            break;
         }
+    }
+    public void timedDrive(double forward, double right, double msToRun)
+    {
+
+        telemetry.update();
+
+        robot.frontRightDrive.setPower(forward + right);
+        robot.frontLeftDrive.setPower(forward - right);
+        robot.rearRightDrive.setPower(forward - right);
+        robot.rearLeftDrive.setPower(forward + right);
+        intRunTime = runtime.milliseconds() + msToRun;
+        while (runtime.milliseconds() < intRunTime) {
+            telemetry.addData("drive-l", forward);
+            telemetry.addData("drive-r", right);
+            telemetry.addData("time", runtime.milliseconds());
+            telemetry.update();
+            idle();
+        }
+        robot.frontRightDrive.setPower(0.0);
+        robot.frontLeftDrive.setPower(0.0);
+        robot.rearRightDrive.setPower(0.0);
+        robot.rearLeftDrive.setPower(0.0);
     }
 }
