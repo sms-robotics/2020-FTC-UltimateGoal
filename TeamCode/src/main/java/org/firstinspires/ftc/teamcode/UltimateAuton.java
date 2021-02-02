c1c786e2305d4705f61e837d17008589e3569e90
Joel Sieh <joel.sieh@gmail.com>
2021-02-01 21:39:57 -0500

    Added a driveDistance() method and a turn() method. Refactored a bit.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UltimateAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UltimateAuton.java
@@ -3,17 +3,18 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 @Autonomous(name="Auton", group="Pushbot")
 public class UltimateAuton extends LinearOpMode {
 
+    private static final double TURN_TICKS_PER_DEGREE = 14.5;
+    private static final double TICKS_PER_MILLIMETER = 8.0;
+    private static final double TURN_POWER = 0.5;
+    private static final double DRIVE_DISTANCE_POWER = 0.5;
+
     /* Declare OpMode members. */
     UltimateHardware robot = new UltimateHardware();   // Use a Pushbot's hardware
-    double forward = 0.5;
-    double right = 0;
-    double intRunTime = 0;
-    int msToRun = 2500;
-
     ElapsedTime runtime = new ElapsedTime();
 
     @Override
@@ -34,22 +35,68 @@ public class UltimateAuton extends LinearOpMode {
 
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
-            timedDrive(forward, right, msToRun);
-            timedDrive(0, 0, 1000);
-            timedDrive(forward*-1, right, msToRun);
+//            driveForTime(0.5, 0, 1000);
+//            turn(90);
+//            driveForTime(0.5, 0, 1000);
+//            turn(90);
+//            driveForTime(0.5, 0, 1000);
+//            turn(90);
+//            driveForTime(0.5, 0, 1000);
+//            turn(90);
+
+            driveDistance(100, 0);
+            turn(90);
+            driveDistance(100, 0);
+            turn(90);
+            driveDistance(100, 0);
+            turn(90);
+            driveDistance(100, 0);
+            turn(90);
             break;
         }
     }
-    public void timedDrive(double forward, double right, double msToRun)
-    {
 
+    public void driveDistance(double distanceMm, double angle)
+    {
+        setRunToPositionMode();
+        telemetry.addData("Drive for distance: ", distanceMm);
+        telemetry.update();
+
+        // TODO: Use real angle - Starting out with angle == 0
+        int targetPositionForward = (int)(distanceMm * TICKS_PER_MILLIMETER);
+        int targetPositionRight = 0;
+        int frontRightTarget = robot.frontRightDrive.getCurrentPosition() + targetPositionForward + targetPositionRight;
+        int frontLeftTarget = robot.frontLeftDrive.getCurrentPosition() + targetPositionForward - targetPositionRight ;
+        int rearRightTarget = robot.rearRightDrive.getCurrentPosition() + targetPositionForward - targetPositionRight;
+        int rearLeftTarget = robot.rearLeftDrive.getCurrentPosition() + targetPositionForward - targetPositionRight;
+
+        robot.frontRightDrive.setTargetPosition(frontRightTarget);
+        robot.frontLeftDrive.setTargetPosition(frontLeftTarget);
+        robot.rearRightDrive.setTargetPosition(rearRightTarget);
+        robot.rearLeftDrive.setTargetPosition(rearLeftTarget);
+
+        robot.frontRightDrive.setPower(DRIVE_DISTANCE_POWER);
+        robot.frontLeftDrive.setPower(DRIVE_DISTANCE_POWER);
+        robot.rearRightDrive.setPower(DRIVE_DISTANCE_POWER);
+        robot.rearLeftDrive.setPower(DRIVE_DISTANCE_POWER);
+
+        while (opModeIsActive() && (robot.frontRightDrive.isBusy() || robot.frontLeftDrive.isBusy() || robot.rearRightDrive.isBusy() || robot.rearLeftDrive.isBusy() )) {
+            // Update telemetry & Allow time for other processes to run
+            telemetry.update();
+            idle();
+        }
+    }
+
+    public void driveForTime(double forward, double right, double msToRun)
+    {
+        setRunUsingEncoderMode();
         telemetry.update();
 
         robot.frontRightDrive.setPower(forward + right);
         robot.frontLeftDrive.setPower(forward - right);
         robot.rearRightDrive.setPower(forward - right);
         robot.rearLeftDrive.setPower(forward + right);
-        intRunTime = runtime.milliseconds() + msToRun;
+        double intRunTime = runtime.milliseconds() + msToRun;
         while (runtime.milliseconds() < intRunTime) {
             telemetry.addData("drive-l", forward);
             telemetry.addData("drive-r", right);
@@ -62,4 +109,49 @@ public class UltimateAuton extends LinearOpMode {
         robot.rearRightDrive.setPower(0.0);
         robot.rearLeftDrive.setPower(0.0);
     }
+
+    public void turn(double degrees) {
+        telemetry.addData("Turning ", degrees);
+        telemetry.addData("encoder pos ", robot.frontRightDrive.getCurrentPosition());
+        telemetry.update();
+
+        setRunToPositionMode();
+        int targetPosition = (int)(degrees * TURN_TICKS_PER_DEGREE);
+        int frontRightTarget = robot.frontRightDrive.getCurrentPosition() - (int)(targetPosition);
+        int frontLeftTarget = robot.frontLeftDrive.getCurrentPosition() + (int)(targetPosition);
+        int rearRightTarget = robot.rearRightDrive.getCurrentPosition() - (int)(targetPosition);
+        int rearLeftTarget = robot.rearLeftDrive.getCurrentPosition() + (int)(targetPosition);
+
+        robot.frontRightDrive.setTargetPosition(frontRightTarget);
+        robot.frontLeftDrive.setTargetPosition(frontLeftTarget);
+        robot.rearRightDrive.setTargetPosition(rearRightTarget);
+        robot.rearLeftDrive.setTargetPosition(rearLeftTarget);
+
+        // TODO: Ease into movement instead of going directly to max power.
+        robot.frontRightDrive.setPower(TURN_POWER);
+        robot.frontLeftDrive.setPower(TURN_POWER);
+        robot.rearRightDrive.setPower(TURN_POWER);
+        robot.rearLeftDrive.setPower(TURN_POWER);
+
+        while (opModeIsActive() && (robot.frontRightDrive.isBusy() || robot.frontLeftDrive.isBusy() || robot.rearRightDrive.isBusy() || robot.rearLeftDrive.isBusy() )) {
+            // Update telemetry & Allow time for other processes to run
+            telemetry.update();
+            idle();
+        }
+    }
+
+    public void setRunUsingEncoderMode() {
+        robot.frontRightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.frontLeftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.rearRightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.rearLeftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    public void setRunToPositionMode() {
+        robot.frontRightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.frontLeftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.rearRightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.rearLeftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+    }
+
 }
